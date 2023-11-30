4068cb921ec4bf823fe31e2bae2274d30b6b2490
Santiago Forero <santiago.forero-y@mail.escuelaing.edu.co>
2023-11-29 19:56:38 -0500

    cambios para trabajar en la nube

--- src/main/java/edu/eci/arsw/magicBrushstrokes/controller/CustomCorsConfiguration.java
+++ src/main/java/edu/eci/arsw/magicBrushstrokes/controller/CustomCorsConfiguration.java
@@ -18,7 +18,7 @@ public class CustomCorsConfiguration {
         // Permite solicitudes desde el origen de tu aplicación en http://192.168.1.11:3000
 
         // Cambiar al momento de subir a azure
-        config.addAllowedOrigin("http://192.168.1.11:3000");
+        config.addAllowedOrigin("https://magicbrushstrokeswebappfront.azurewebsites.net");
 
         // Configura otras opciones CORS según tus necesidades
         config.addAllowedMethod("GET");
