54b3049a5f9985cdc2a7d5fc29545fa9bd52b40b
Santiago Forero <santiago.forero-y@mail.escuelaing.edu.co>
2023-12-02 18:37:12 -0500

    revision politicas push

--- src/main/java/edu/eci/arsw/magicBrushstrokes/controller/CustomCorsConfiguration.java
+++ src/main/java/edu/eci/arsw/magicBrushstrokes/controller/CustomCorsConfiguration.java
@@ -18,7 +18,7 @@ public class CustomCorsConfiguration {
         // Permite solicitudes desde el origen de tu aplicación en http://192.168.1.11:3000
 
         // Cambiar al momento de subir a azure
-        config.addAllowedOrigin("https://magicbrushstrokeswebappfront.azurewebsites.net");
+        config.addAllowedOrigin("*");
 
         // Configura otras opciones CORS según tus necesidades
         config.addAllowedMethod("GET");
