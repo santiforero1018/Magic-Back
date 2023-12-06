c3a5f97a7a9a41b5d315cfc05b3ca6afa80c676c
Santiago Forero <santiago.forero-y@mail.escuelaing.edu.co>
2023-12-05 22:28:04 -0500

    configuracion en el websocket

--- src/main/java/edu/eci/arsw/magicBrushstrokes/controller/MagicBrushWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/magicBrushstrokes/controller/MagicBrushWebSocketConfig.java
@@ -3,14 +3,14 @@ package edu.eci.arsw.magicBrushstrokes.controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class MagicBrushWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
+public class MagicBrushWebSocketConfig implements WebSocketMessageBrokerConfigurer{
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/game");
@@ -19,7 +19,7 @@ public class MagicBrushWebSocketConfig extends AbstractWebSocketMessageBrokerCon
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompendpoint").setAllowedOrigins("https://magicbrushstrokeswebappfront.azurewebsites.net").withSockJS(); // Cambiar al momento de subir a azure
+        registry.addEndpoint("/stompendpoint").setAllowedOrigins("https://magicbrushstrokeswebappfront.azurewebsites.net", "http://magicbrushstrokeswebappfront.azurewebsites.net").withSockJS(); // Cambiar al momento de subir a azure
         
     }
 }
