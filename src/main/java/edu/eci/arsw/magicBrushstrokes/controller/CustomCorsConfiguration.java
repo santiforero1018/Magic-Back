67eee98649bdbdcdac671ff3aaa0d55e322d6543
Juan Sebastian Cepeda <juan.cepeda-s@mail.escuelaing.edu.co>
2023-12-12 00:49:58 -0500

    corrección de errores

--- src/main/java/edu/eci/arsw/magicBrushstrokes/controller/CustomCorsConfiguration.java
+++ src/main/java/edu/eci/arsw/magicBrushstrokes/controller/CustomCorsConfiguration.java
@@ -1,31 +1,31 @@
-package edu.eci.arsw.magicBrushstrokes.controller;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-@Configuration
-public class CustomCorsConfiguration {
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        // Permite solicitudes desde el origen de tu aplicación en http://192.168.1.11:3000
+// package edu.eci.arsw.magicBrushstrokes.controller;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+// import org.springframework.web.cors.CorsConfigurationSource;
+// import org.springframework.web.filter.CorsFilter;
+// @Configuration
+// public class CustomCorsConfiguration {
+//     @Bean
+//     public CorsFilter corsFilter() {
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//         CorsConfiguration config = new CorsConfiguration();
+//         // Permite solicitudes desde el origen de tu aplicación en http://192.168.1.11:3000
 
-        // Cambiar al momento de subir a azure
-        config.addAllowedOrigin("http://localhost:3000");
+//         // Cambiar al momento de subir a azure
+//         config.addAllowedOrigin("http://localhost:3000");
 
-        // Configura otras opciones CORS según tus necesidades
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PUT");
-        config.addAllowedHeader("Content-Type");
-        config.setAllowCredentials(true);
-        source.registerCorsConfiguration("/**", config);
+//         // Configura otras opciones CORS según tus necesidades
+//         config.addAllowedMethod("GET");
+//         config.addAllowedMethod("POST");
+//         config.addAllowedMethod("PUT");
+//         config.addAllowedHeader("Content-Type");
+//         config.setAllowCredentials(true);
+//         source.registerCorsConfiguration("/**", config);
 
-        return new CorsFilter(source);
-    }
+//         return new CorsFilter(source);
+//     }
 
 
-}
+// }
